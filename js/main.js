06967009c4b9f6580b5f0ee0e42bb3c93d48a165
purcy <purcyguo@gmail.com>
2018-09-01 19:01:31 +0800

    add loading

---
+++ js/main.js
@@ -0,0 +1,12 @@
+welcome.classList.remove('active');
+
+
+portfolioAll.addEventListener("click", function () {
+    portfolioSelect.className = "selectBar state-pa";
+});
+portfolioFramework.addEventListener("click", function () {
+    portfolioSelect.className = "selectBar state-pf";
+});
+portfolioVallia.addEventListener("click", function () {
+    portfolioSelect.className = "selectBar state-pv";
+});
